fdd10f0b62391ede3e4227cd17a58f4186db22f3
briangabini <briangabini@gmail.com>
2024-03-19 23:08:15 +0800

    feat: add a length parameter for daxpy()

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
+// directives
+#define MAX 134217728
+
 extern double asmfunc(double A, double X, double Y);
 
-static double* daxpy(double A, double X[], double Y[], int mode);
+static double* daxpy(int n, double A, double X[], double Y[], int mode);
 
 int main() {
+	const int n = 3;
 	const double A = 2.0;
 	double X[] = {1.0, 2.0, 3.0};
 	double Y[] = {4.0, 5.0, 6.0};
-	double *Z1 = daxpy(A, X, Y, 0);
-	double *Z2 = daxpy(A, X, Y, 1);
+	double *Z1 = daxpy(n, A, X, Y, 0);
+	double *Z2 = daxpy(n, A, X, Y, 1);
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < n; i++) {
 		printf("%f\n", Z1[i]);
 	}
 
 	printf("\n");
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < n; i++) {
 		printf("%f\n", Z2[i]);
 	}
 
@@ -25,20 +29,16 @@ int main() {
 }
 
 // Function to perform daxpy operation
-static double* daxpy(double A, double X[], double Y[], int mode) {
+static double* daxpy(int n, double A, double X[], double Y[], int mode) {
+	static double Z[MAX];
 
-	int size = 3;
-	static double Z[3];		// set to static to have a longer lifetime
-
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < n; i++) {
 
 		if (mode == 0) {
 			Z[i] = A * X[i] + Y[i];
 		}
 		else {
-			double tempX = X[i];
-			double tempY = Y[i];
-			Z[i] = asmfunc(A, tempX, tempY);
+			Z[i] = asmfunc(A, X[i], Y[i]);
 		}
 	}
 

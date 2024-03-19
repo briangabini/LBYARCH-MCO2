956e2e2e4ea88361adbe398aeb182c0659ea4df1
briangabini <briangabini@gmail.com>
2024-03-19 22:46:23 +0800

    feat: implement daxpy function in c and in asm

--- asmfunc.asm
+++ asmfunc.asm
@@ -1,17 +1,9 @@
-
-section .data
-	msg db "Hello, World!", 13, 10, 0
-
 section .text
 bits 64
 default rel
 
 global asmfunc
-extern printf
-
 asmfunc: 
-	sub rsp, 8*5	; caller
-	mov rcx, msg
-	call printf
-	add rsp, 8*5
-	ret
+mulsd xmm0, xmm1 ; A * B
+addsd xmm0, xmm2 ; A * B + C
+ret
